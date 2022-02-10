8429941a24cf769a4b428b756a26efbf85696f8f
Rory Coffey <coffeyrt@gmail.com>
2022-02-09 19:27:59 -0500

    moved to separate files

--- ethereum/front_end/src/App.tsx
+++ ethereum/front_end/src/App.tsx
@@ -1,19 +1,26 @@
-import { DAppProvider, Ropsten } from "@usedapp/core"
-import { Header } from "./components/Header"
-import Container from "@mui/material/Container"
-import { Main } from "./components/Main"
+import { DAppProvider, Ropsten } from "@usedapp/core";
+import { Header } from "./components/Header";
+import Container from "@mui/material/Container";
+import { Balances } from "./components/Balances";
+import { SendAppreciation } from "./components/SendAppreciation";
+import { Admin } from "./components/Admin";
+import { Contract } from "@ethersproject/contracts";
+import swap from "./Swap.json";
 
 const config = {
   networks: [Ropsten],
-}
+};
 
 function App() {
+
   return (
     <DAppProvider config={config}>
-        <Header />
-        <Container maxWidth="sm">
-            <Main />
-        </Container>
+      <Header />
+      <Container maxWidth="sm">
+        <Balances />
+        <SendAppreciation />
+        <Admin />
+      </Container>
     </DAppProvider>
   );
 }
