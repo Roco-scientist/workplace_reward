e162a3dfc15408925820118a148cb753ff08071a
Rory Coffey <coffeyrt@gmail.com>
2022-02-10 13:28:17 -0500

    removed virtual from contract decimal function

--- ethereum/contracts/ThankYouToken.sol
+++ ethereum/contracts/ThankYouToken.sol
@@ -5,10 +5,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ThankYouToken is ERC20 {
     constructor(uint256 supply) public ERC20("Thank You Token", "TY") {
-        _mint(msg.sender, supply); // 1 million tokens with 18 deecimal digits
+        _mint(msg.sender, supply);
     }
 
-    function decimals() public view virtual override returns (uint8) {
+    function decimals() public view override returns (uint8) {
         return 2;
     }
 }
