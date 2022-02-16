604d039e3999a58e56ac2d39df47f5f97ce7bbfc
Rory Coffey <coffeyrt@gmail.com>
2022-02-16 14:28:12 -0500

    prevent the need to open db everytime there is a call

--- ethereum/api/src/db/index.ts
+++ ethereum/api/src/db/index.ts
@@ -1,9 +1,9 @@
 import sqlite3 from "sqlite3";
-import { open } from "sqlite";
+import { open, Database } from "sqlite";
 import { existsSync, readFile } from "fs";
 // import { assert } from "node";
 
-const dbConnect = async () => {
+export const dbConnect = async () => {
   // assert(existsSync("./worker_rewards.db") == true, "sqlite3 database file missing");
   if (!existsSync("./src/db/worker_rewards.db")) {
     // throw new AssertionError("sqlite3 database file missing");
@@ -44,28 +44,24 @@ export const initialize = async () => {
   });
 };
 
-export const queryUsers = async (address: string) => {
-  const db = await dbConnect();
+export const queryUsers = async (address: string, db: Database) => {
   // console.log("Query user table");
   const otherUsers = await db.all<User[]>(
     `SELECT address, name FROM Users WHERE address != ? AND company IN (SELECT company FROM Users WHERE address = ?)`,
     [address, address]
   );
-  await db.close();
 
   // console.log("Other users sent:");
   // console.log(otherUsers);
   return otherUsers;
 };
 
-export const queryCompliments = async (address: string) => {
-  const db = await dbConnect();
+export const queryCompliments = async (address: string, db: Database) => {
   // console.log("Query compliments table");
   const compliments = await db.all<Compliment[]>(
     `SELECT rowid AS id, message FROM Compliments WHERE company IN (SELECT company FROM Users WHERE address = ?)`,
     [address]
   );
-  await db.close();
 
   // console.log("Compliments sent:");
   // console.log(compliments);
--- ethereum/api/src/index.ts
+++ ethereum/api/src/index.ts
@@ -1,54 +1,66 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { constants } from "ethers";
-import { Compliment, queryCompliments, queryUsers, User } from "./db";
+import {
+  Compliment,
+  queryCompliments,
+  queryUsers,
+  User,
+  dbConnect,
+} from "./db";
 
 // Connection app
 const app = express();
 const port = 3080;
 
-app.use(bodyParser.json());
+dbConnect()
+  .then((db) => {
+    app.use(bodyParser.json());
 
-// Get request for all other users from the same company
-app.get("/api/users", async (req, res) => {
-  // The address of the incoming REST get in order to narrow down the response to only the data
-  // that is connected to the company which the address is coming from
-  const accountAddress = req.query.accountAddress.toString();
+    // Get request for all other users from the same company
+    app.get("/api/users", async (req, res) => {
+      // The address of the incoming REST get in order to narrow down the response to only the data
+      // that is connected to the company which the address is coming from
+      const accountAddress = req.query.accountAddress.toString();
 
-  let otherUsers: User[];
-  // If the incoming account address is not 0000 and is registered then narrow down the users
-  // that are within the same company and send back.  Otherwise, send back an empty list
-  if (accountAddress !== constants.AddressZero) {
-    otherUsers = await queryUsers(accountAddress);
-  } else {
-    otherUsers = [];
-  }
-  // console.log("API other users:");
-  // console.log(otherUsers);
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.json(otherUsers);
-});
+      let otherUsers: User[];
+      // If the incoming account address is not 0000 and is registered then narrow down the users
+      // that are within the same company and send back.  Otherwise, send back an empty list
+      if (accountAddress !== constants.AddressZero) {
+        otherUsers = await queryUsers(accountAddress, db);
+      } else {
+        otherUsers = [];
+      }
+      // console.log("API other users:");
+      // console.log(otherUsers);
+      res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+      res.json(otherUsers);
+    });
 
-// REST response for get the compliments that are associated with the users company
-app.get("/api/compliments", async (req, res) => {
-  // The address of the incoming REST get in order to narrow down the response to only the data
-  // that is connected to the company which the address is coming from
-  const accountAddress = req.query.accountAddress.toString();
+    // REST response for get the compliments that are associated with the users company
+    app.get("/api/compliments", async (req, res) => {
+      // The address of the incoming REST get in order to narrow down the response to only the data
+      // that is connected to the company which the address is coming from
+      const accountAddress = req.query.accountAddress.toString();
 
-  let groupCompliments: Compliment[];
-  if (
-    accountAddress !== constants.AddressZero
-  ) {
-    groupCompliments = await queryCompliments(accountAddress);
-  } else {
-    groupCompliments = [];
-  }
-  // console.log("API compliments:");
-  // console.log(groupCompliments);
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.json(groupCompliments);
-});
+      let groupCompliments: Compliment[];
+      if (accountAddress !== constants.AddressZero) {
+        groupCompliments = await queryCompliments(accountAddress, db);
+      } else {
+        groupCompliments = [];
+      }
+      // console.log("API compliments:");
+      // console.log(groupCompliments);
+      res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+      res.json(groupCompliments);
+    });
 
-app.listen(port, () => {
-  console.log(`Server listening on the port::${port}`);
-});
+    app.listen(port, () => {
+      console.log(`Server listening on the port::${port}`);
+    });
+
+    // process.on("SIGINT", db.close());
+  })
+  .catch((err) => {
+    throw err;
+  });
