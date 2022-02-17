3f542678fa1ccb34c9121411f64367202430d2e6
Rory Coffey <coffeyrt@gmail.com>
2022-02-17 09:46:39 -0500

    added main account

--- ethereum/api/src/db/insert_tests.sql
+++ ethereum/api/src/db/insert_tests.sql
@@ -2,6 +2,7 @@ INSERT INTO Users (address,name, company)
 VALUES 
 ("0x3bD7736bB6feA5ebe2AC8eb7F380D0963D92d473","Jon","1"),
 ("0x6cE09101fcE65B6619606a7e0B91ef85dD99B5e5","Mary","1"),
+("0x839901c21D20316b0DDcA205AAe53A1EbB886cf4","Jefe","1"),
 ("0xE701A32AB9423594a0Dc65f2590C09fAD1D07Ca0","Lucy","1");
 
 INSERT INTO Compliments (message, company)
