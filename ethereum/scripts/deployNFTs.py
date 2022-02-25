146cbf1c90b184da516017a0d77e5d9676ccef95
Rory Coffey <coffeyrt@gmail.com>
2022-02-25 09:59:53 -0500

    added front end update

--- ethereum/scripts/deployNFTs.py
+++ ethereum/scripts/deployNFTs.py
@@ -1,5 +1,7 @@
 from brownie import EmployeeOfTheMonth, network
 from scripts.helpful_scripts import get_account, LOCAL_BLOCKCHAIN_ENVIRONMENTS
+from pathlib import Path
+import shutil
 
 def deploy_EOTM(account):
     eotm_nft = EmployeeOfTheMonth.deploy({"from": account})
@@ -7,7 +9,13 @@ def deploy_EOTM(account):
 
 
 def update_front_end():
-    pass
+    front_end_map = Path("./front_end/src/contract_map.json")
+    contracts_map = Path("./build/deployments/map.json")
+    shutil.copy(contracts_map, front_end_map)
+
+    front_end_eotm = Path("./front_end/src/EmployeeOfTheMonth.json")
+    contracts_eotm = Path("./build/contracts/EmployeeOfTheMonth.json")
+    shutil.copy(contracts_eotm, front_end_eotm)
 
 
 def main():
