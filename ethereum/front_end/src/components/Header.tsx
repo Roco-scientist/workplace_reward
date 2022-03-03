a8e4cf70482aaa55a532c0793aaa15732f1ee9aa
Rory Coffey <coffeyrt@gmail.com>
2022-03-03 07:39:39 -0500

    added register button

--- ethereum/front_end/src/components/Header.tsx
+++ ethereum/front_end/src/components/Header.tsx
@@ -1,6 +1,9 @@
 import { useEthers } from "@usedapp/core";
-import Button from "@mui/material/Button";
+import { Button, Stack } from "@mui/material";
 import { styled } from "@mui/system";
+import { useState } from "react";
+import { constants } from "ethers";
+import { User } from "./Common";
 
 const StyledHeader = styled("div")({
   padding: 8,
@@ -11,20 +14,40 @@ const StyledHeader = styled("div")({
 
 export const Header = () => {
   const { account, activateBrowserWallet, deactivate } = useEthers();
+  const accountAddress = account ? account : constants.AddressZero;
+
+  const [isRegistered, setIsRegisterd] = useState(false);
+  fetch("http://localhost:3080/api/users/all?accountAddress=" + accountAddress)
+    .then((response) => response.json())
+    .then((response: User[]) => {
+      if (account) {
+        const accountUser = response.filter((user) => user.address === account);
+        if (accountUser.length > 0) {
+          setIsRegisterd(true);
+        } else {
+          setIsRegisterd(false);
+        }
+      } else {
+        setIsRegisterd(false);
+      }
+    });
 
   const isConnected = account !== undefined;
 
   return (
     <StyledHeader>
-      {isConnected ? (
-        <Button variant="contained" onClick={deactivate}>
-          Disconnect
-        </Button>
-      ) : (
-        <Button variant="contained" onClick={() => activateBrowserWallet()}>
-          Connect
-        </Button>
-      )}
+      <Stack direction="row" spacing={2}>
+        {!isRegistered && isConnected ? <Button variant="contained">Register</Button> : <div></div>}
+        {isConnected ? (
+          <Button variant="contained" onClick={deactivate}>
+            Disconnect
+          </Button>
+        ) : (
+          <Button variant="contained" onClick={() => activateBrowserWallet()}>
+            Connect
+          </Button>
+        )}
+      </Stack>
     </StyledHeader>
   );
 };
