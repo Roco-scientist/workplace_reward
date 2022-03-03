1f16edf319eb1817bbc58e9f1dcf2345ddbb0785
Rory Coffey <coffeyrt@gmail.com>
2022-03-02 20:36:52 -0500

    moved to different db format

--- ethereum/api/src/db/index.ts
+++ ethereum/api/src/db/index.ts
@@ -1,89 +1,43 @@
 import sqlite3 from "sqlite3";
 import { open, Database } from "sqlite";
 import { existsSync, readFile } from "fs";
+import crypto from "crypto";
 // import { assert } from "node";
+//
 
-export const dbConnect = async () => {
-  // assert(existsSync("./worker_rewards.db") == true, "sqlite3 database file missing");
-  if (!existsSync("./src/db/worker_rewards.db")) {
-    // throw new AssertionError("sqlite3 database file missing");
-    throw "sqlite3 database file missing";
-  }
-  const db = await open({
-    filename: "./src/db/worker_rewards.db",
-    driver: sqlite3.Database,
-  });
-  return db;
-};
+const db = new sqlite3.Database("./src/db/worker_rewards.db");
 
-// User information that is used to send thanks for rewards
-export interface User {
-  id: number;
-  firstName: string;
-  lastName: string;
-  address: string;
-}
-
-// Compliments
-export interface Compliment {
-  id: number;
-  message: string;
-}
-
-export const initialize = async () => {
-  readFile(".src/db/initialize_db.sql", async (err, buff) => {
-    if (err) {
-      throw err;
-    }
-
-    const db = await dbConnect();
-    db.run(buff.toString(), [], (err) => {
-      if (err) {
-        throw err;
-      }
-    });
-    await db.close();
-  });
-};
-
-export const queryUsers = async (address: string, db: Database, all: boolean) => {
-  // console.log("Query user table");
-  let otherUsers = await db.all<User[]>(
-    `SELECT rowid AS id, firstName, lastName, address FROM Users WHERE company IN (SELECT company FROM Users WHERE address = ?) ORDER BY lastName`,
-    [address]
+db.serialize(() => {
+  db.run(
+    "CREATE TABLE IF NOT EXISTS Users ( \
+    address VARCHAR(60) PRIMARY KEY NOT NULL, \
+    firstName VARCHAR(20) NOT NULL, \
+    lastName VARCHAR(20) NOT NULL, \
+    company VARCHAR(100) NOT NULL, \
+    hashed_password BLOB, \
+    salt BLOB \
+     )"
   );
-  
-  if (!all) {
-    otherUsers = otherUsers.filter((user) => user.address != address);
-  }
-
-  // console.log("Other users sent:");
-  // console.log(otherUsers);
-  return otherUsers;
-};
-
-export const queryCompliments = async (address: string, db: Database) => {
-  // console.log("Query compliments table");
-  const compliments = await db.all<Compliment[]>(
-    `SELECT rowid AS id, message FROM Compliments WHERE company IN (SELECT company FROM Users WHERE address = ?)`,
-    [address]
+  db.run(
+    "CREATE TABLE IF NOT EXISTS Compliments ( \
+    message VARCHAR(100) NOT NULL, \
+    company VARCHAR(100) NOT NULL \
+    )"
   );
 
-  // console.log("Compliments sent:");
-  // console.log(compliments);
-  return compliments;
-};
+  // create an initial user (username: alice, password: letmein)
+  const salt = crypto.randomBytes(16);
+  db.run(
+    "INSERT OR IGNORE INTO users (address, firstName, lastName, company, hashed_password, salt) VALUES (?, ?, ?, ?, ?, ?)",
+    [
+      "0x3bD7736bB6feA5ebe2AC8eb7F380D0963D92d473",
+      "Jon",
+      "Smith",
+      "1",
+      crypto.pbkdf2Sync("letmein", salt, 310000, 32, "sha256"),
+      salt,
+    ]
+  );
+});
 
-// export const addUser = await (address: string, name: string, company: string) => {
-//   const db = await dbConnect();
-//   db.run(
-//     `INSERT INTO Users (address,name,company) VALUES (?,?,?)`,
-//     [address, name, company],
-//     (err) => {
-//       if (err) {
-//         throw err;
-//       }
-//     }
-//   );
-//   await db.close();
-// };
+export = db;
--- ethereum/api/src/index.ts
+++ ethereum/api/src/index.ts
@@ -4,16 +4,27 @@ import { constants } from "ethers";
 import IPFS from "ipfs-core";
 import { BufferList } from "bl";
 // import makeIpfsFetch from "ipfs-fetch";
-import {
-  Compliment,
-  queryCompliments,
-  queryUsers,
-  User,
-  dbConnect,
-} from "./db";
+import db from "./db";
 import dotenv from "dotenv";
 import cors from "cors";
 import pinataSDK from "@pinata/sdk";
+import passport from "passport";
+import LocalStrategy from "passport-local";
+import crypto from "crypto";
+
+// User information that is used to send thanks for rewards
+export interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  address: string;
+}
+
+// Compliments
+export interface Compliment {
+  id: number;
+  message: string;
+}
 
 // Connection app
 const app = express();
@@ -25,131 +36,179 @@ const pinata = pinataSDK(
   process.env.PINATA_SECRET_API_KEY
 );
 
-dbConnect()
-  .then((db) => {
-    app.use(bodyParser.json());
-    app.use(cors({ origin: "http://localhost:3000" }));
+passport.use(
+  new LocalStrategy(function verify(accountAddress, password, cb) {
+    db.get(
+      "SELECT rowid AS id, * FROM users WHERE address = ?",
+      [accountAddress],
+      function (err, row) {
+        if (err) {
+          return cb(err);
+        }
+        if (!row) {
+          return cb(null, false, {
+            message: "Incorrect username or password.",
+          });
+        }
 
-    // Get request for all other users from the same company
-    app.get("/api/users/other", async (req, res) => {
-      // The address of the incoming REST get in order to narrow down the response to only the data
-      // that is connected to the company which the address is coming from
-      const accountAddress = req.query.accountAddress.toString();
-
-      let otherUsers: User[];
-      // If the incoming account address is not 0000 and is registered then narrow down the users
-      // that are within the same company and send back.  Otherwise, send back an empty list
-      if (accountAddress !== constants.AddressZero) {
-        otherUsers = await queryUsers(accountAddress, db, false);
-      } else {
-        otherUsers = [];
+        crypto.pbkdf2(
+          password,
+          row.salt,
+          310000,
+          32,
+          "sha256",
+          function (err, hashedPassword) {
+            if (err) {
+              return cb(err);
+            }
+            if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+              return cb(null, false, {
+                message: "Incorrect username or password.",
+              });
+            }
+            return cb(null, row);
+          }
+        );
       }
-      // console.log("API other users:");
-      // console.log(otherUsers);
-      res.json(otherUsers);
-    });
-
-    // Get request for all users from the same company
-    app.get("/api/users/all", async (req, res) => {
-      // The address of the incoming REST get in order to narrow down the response to only the data
-      // that is connected to the company which the address is coming from
-      const accountAddress = req.query.accountAddress.toString();
-
-      let allUsers: User[];
-      // If the incoming account address is not 0000 and is registered then narrow down the users
-      // that are within the same company and send back.  Otherwise, send back an empty list
-      if (accountAddress !== constants.AddressZero) {
-        allUsers = await queryUsers(accountAddress, db, true);
-      } else {
-        allUsers = [];
-      }
-      // console.log("API other users:");
-      // console.log(otherUsers);
-      res.json(allUsers);
-    });
-
-    // REST response for get the compliments that are associated with the users company
-    app.get("/api/compliments", async (req, res) => {
-      // The address of the incoming REST get in order to narrow down the response to only the data
-      // that is connected to the company which the address is coming from
-      const accountAddress = req.query.accountAddress.toString();
-
-      let groupCompliments: Compliment[];
-      if (accountAddress !== constants.AddressZero) {
-        groupCompliments = await queryCompliments(accountAddress, db);
-      } else {
-        groupCompliments = [];
-      }
-      // console.log("API compliments:");
-      // console.log(groupCompliments);
-      res.json(groupCompliments);
-    });
-
-    app.post("/api/ipfsjson", async (req, res) => {
-      const jsonBody = req.body;
-
-      const jsonName = jsonBody.description.replaceAll(" ", "_") + ".json";
-
-      const options = {
-        pinataMetadata: {
-          name: jsonName,
-        },
-      };
-      pinata
-        .pinJSONToIPFS(jsonBody, options)
-        .then((result) => {
-          res.json(result)
-        })
-        .catch((err) => {
-          res.json(err);
-        });
-    });
-
-    // Below is still a work in progress
-    app.get("/api/getnft", async (req, res) => {
-      const cid = req.query.cid.toString();
-      const ipfs = await IPFS.create();
-      let text = "";
-      for await (const file of ipfs.get(cid)) {
-        console.log(file.toString());
-        text += file.toString();
-
-        // const content = new BufferList();
-        // for await (const chunk of file.content) {
-        //   content.append(chunk);
-        // }
-
-        // console.log(content.toString());
-      }
-      // for await (const chunk of ipfs.cat(cid)) {
-      //   chunks.push(chunk);
-      // }
-      // const text = Buffer.concat(chunks).toString();
-      console.log(text);
-      // const fetch = makeIpfsFetch({ ipfs });
-      // const response = await fetch("ipfs://" + cid);
-      // const text = response.text();
-      // let content = "";
-      // for await (const file of ipfs.get(cid)) {
-      // const content = [];
-      // if (file.content) {
-      //   for await (const chunk of file.content) {
-      //     content.push(chunk);
-      //   }
-      // }
-      // content += file.toString();
-      // }
-      // console.log(text);
-      res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-      res.json(text);
-    });
-
-    app.listen(port, () => {
-      console.log(`Server listening on the port::${port}`);
-    });
-
-    // process.on("SIGINT", db.close());
+    );
   })
-  .catch((err) => {
-    throw err;
-  });
+);
+
+app.use(bodyParser.json());
+app.use(cors({ origin: "https://localhost:3000" }));
+
+// Get request for all other users from the same company
+app.get("/api/users/other", async (req, res) => {
+  // The address of the incoming REST get in order to narrow down the response to only the data
+  // that is connected to the company which the address is coming from
+  const accountAddress = req.query.accountAddress.toString();
+
+  // If the incoming account address is not 0000 and is registered then narrow down the users
+  // that are within the same company and send back.  Otherwise, send back an empty list
+  if (accountAddress !== constants.AddressZero) {
+    db.all(
+      `SELECT rowid AS id, firstName, lastName, address FROM Users WHERE company IN (SELECT company FROM Users WHERE address = ?) AND address != ? ORDER BY lastName`,
+      [accountAddress, accountAddress],
+      (err, rows) => {
+        if (err) {
+          res.json(err);
+        } else {
+          res.json(rows);
+        }
+      }
+    );
+  } else {
+    res.json([]);
+  }
+});
+
+// Get request for all users from the same company
+app.get("/api/users/all", async (req, res) => {
+  // The address of the incoming REST get in order to narrow down the response to only the data
+  // that is connected to the company which the address is coming from
+  const accountAddress = req.query.accountAddress.toString();
+
+  if (accountAddress !== constants.AddressZero) {
+    db.all(
+      `SELECT rowid AS id, firstName, lastName, address FROM Users WHERE company IN (SELECT company FROM Users WHERE address = ?) ORDER BY lastName`,
+      [accountAddress],
+      (err, rows) => {
+        if (err) {
+          res.json(err);
+        } else {
+          res.json(rows);
+        }
+      }
+    );
+  } else {
+    res.json([]);
+  }
+});
+
+// REST response for get the compliments that are associated with the users company
+app.get("/api/compliments", async (req, res) => {
+  // The address of the incoming REST get in order to narrow down the response to only the data
+  // that is connected to the company which the address is coming from
+  const accountAddress = req.query.accountAddress.toString();
+
+  if (accountAddress !== constants.AddressZero) {
+    db.all(
+      `SELECT rowid AS id, message FROM Compliments WHERE company IN (SELECT company FROM Users WHERE address = ?)`,
+      [accountAddress],
+      (err, rows: Compliment[]) => {
+        if (err) {
+          res.json(err);
+        } else {
+          res.json(rows);
+        }
+      }
+    );
+  } else {
+    res.json([]);
+  }
+});
+
+app.post("/api/ipfsjson", async (req, res) => {
+  const jsonBody = req.body;
+
+  const jsonName = jsonBody.description.replaceAll(" ", "_") + ".json";
+
+  const options = {
+    pinataMetadata: {
+      name: jsonName,
+    },
+  };
+  pinata
+    .pinJSONToIPFS(jsonBody, options)
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// Below is still a work in progress
+app.get("/api/getnft", async (req, res) => {
+  const cid = req.query.cid.toString();
+  const ipfs = await IPFS.create();
+  let text = "";
+  for await (const file of ipfs.get(cid)) {
+    console.log(file.toString());
+    text += file.toString();
+
+    // const content = new BufferList();
+    // for await (const chunk of file.content) {
+    //   content.append(chunk);
+    // }
+
+    // console.log(content.toString());
+  }
+  // for await (const chunk of ipfs.cat(cid)) {
+  //   chunks.push(chunk);
+  // }
+  // const text = Buffer.concat(chunks).toString();
+  console.log(text);
+  // const fetch = makeIpfsFetch({ ipfs });
+  // const response = await fetch("ipfs://" + cid);
+  // const text = response.text();
+  // let content = "";
+  // for await (const file of ipfs.get(cid)) {
+  // const content = [];
+  // if (file.content) {
+  //   for await (const chunk of file.content) {
+  //     content.push(chunk);
+  //   }
+  // }
+  // content += file.toString();
+  // }
+  // console.log(text);
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.json(text);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on the port::${port}`);
+});
+
+// process.on("SIGINT", db.close());
